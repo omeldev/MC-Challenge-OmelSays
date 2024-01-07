b6ebd019457bc8c59e0218e9535341a455f85221
Omel <mail@omel.dev>
2024-01-07 02:33:54 +0100

    Update KillMob.java

--- src/main/java/dev/omel/says/task/tasks/KillMob.java
+++ src/main/java/dev/omel/says/task/tasks/KillMob.java
@@ -1,6 +1,8 @@
 package dev.omel.says.task.tasks;
 
 import dev.omel.says.task.ChallengeTask;
+import dev.omel.says.task.player.TaskPlayer;
+import org.bukkit.entity.Player;
 import org.bukkit.entity.Zombie;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -16,9 +18,12 @@ public class KillMob extends ChallengeTask implements Listener {
 
     @EventHandler
     public void onKillMob(EntityDeathEvent event) {
-        if (event.getEntity() instanceof Zombie)
-            consumer.accept(true);
-        else consumer.accept(false);
+        if (event.getEntity().getKiller() == null) return;
+        if (!(event.getEntity().getKiller() instanceof Player)) return;
+        if (event.getEntity() instanceof Zombie) consumer.accept(true, event.getEntity().getKiller());
+        else consumer.accept(false, event.getEntity().getKiller());
+
+
     }
 
 
